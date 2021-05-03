2242b9ba8127e5d669be07763c978c3952be1d64
Elias Howell <mailbox.ejh@gmail.com>
2021-05-03 06:55:47 -0500

    Update germination.py

--- germination.py
+++ germination.py
@@ -3,14 +3,14 @@ import pyquery, os, sys, validators, subprocess
 def main():
 	add_to_client(confirm_magnets(get_url()))
 
-#Gets valid url from user
+# Gets valid url from user
 def get_url():
 	url = input("\nCopy and paste a url to scrape magnets from: ").strip()
 	while validators.url(url) != True:
 		url = input("\nInvalid url. Please enter a valid url i.e. \"https://google.com\": ").strip()
 	return url
 
-#Confirms that the provided url contains magnets and appends to confirmed magnet list
+# Confirms that the provided url contains magnets and appends to confirmed magnet list
 def confirm_magnets(url):
 	pq = pyquery.PyQuery(url)
 	confirmed = list()
@@ -22,12 +22,12 @@ def confirm_magnets(url):
 		quit("\nNo magnets found. Exiting...")
 	return confirmed
 
-#Adds magnet links to system's default torrent client
+# Adds magnet links to system's default torrent client
 def add_to_client(magnets):
 	for i in magnets:
-		if sys.platform.startswith('linux'): #Linux
+		if sys.platform.startswith('linux'): # Linux
 			os.system("xdg-open " + i)
-		elif sys.platform.startswith('win32'): #Windows
+		elif sys.platform.startswith('win32'): # Windows
 			os.startfile(i)
 
 main()
